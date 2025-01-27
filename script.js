3b0c79dbb5b328d05401a961ff9f507124084200
Ning <405429005@qq.com>
2025-01-27 21:05:38 +0800

    缩小 logo，添加已选择时间段的显示

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ document.getElementById('times').addEventListener('change', function() {
   const selectedTimes = Array.from(this.selectedOptions).map(option => option.text); // 显示中文
   document.getElementById('time-list').innerText = selectedTimes.join(', ');
 });
+
+document.getElementById('time-slots').addEventListener('change', function() {
+  const selectedTimeSlots = Array.from(this.selectedOptions).map(option => option.text); // 显示中文
+  document.getElementById('time-slot-list').innerText = selectedTimeSlots.join(', ');
+});
