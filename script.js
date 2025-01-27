a297297cf41c6cec3790953b918ecca2b27b7da8
Ning <405429005@qq.com>
2025-01-27 21:02:57 +0800

    缩小 logo，调整已选择科目和时间的显示位置

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('subjects').addEventListener('change', function() {
-  const selectedSubjects = Array.from(this.selectedOptions).map(option => option.value);
+  const selectedSubjects = Array.from(this.selectedOptions).map(option => option.text); // 显示中文
   document.getElementById('subject-list').innerText = selectedSubjects.join(', ');
 });
 
 document.getElementById('times').addEventListener('change', function() {
-  const selectedTimes = Array.from(this.selectedOptions).map(option => option.value);
+  const selectedTimes = Array.from(this.selectedOptions).map(option => option.text); // 显示中文
   document.getElementById('time-list').innerText = selectedTimes.join(', ');
 });
